40484a2ff8a89da9a1bf94b25493d9d61b2dc56c
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-20 16:38:38 +0200

    add pseudocodifica

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,11 @@
+/*
+Creare un prompt dove l'utente sceglie pari o dispari
+creare un prompt dove l'utente sceglie un numero compreso tra 1 e 5 inclusi
+creare una funzione per generare un numero random da 1 a 5 e salvarlo in una variabile
+creare una variabile in cui inserire la somma del numero inserito dall'utente e numero random
+creare una funzione per stabilire se numero è pari o dispari
+SE numero utente > numero computer 
+    stampa L'utente ha vinto
+ALTRIEMENTI 
+    stampa Il computer ha vinto
+*/
