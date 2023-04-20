4e95f8f5e35021f15e53bd856b8f84eed5590ac8
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-20 15:51:59 +0200

    add console.log for result

---
+++ Palindroma/script.js
@@ -0,0 +1,34 @@
+/*
+
+chiedere all'utente di inserire una parola con un prompt
+creare una funzione di controllo 
+
+*/
+
+const wordReverse = prompt('inserisci una parola e ti dirò se è palindroma!');
+
+
+function reverseString(str) {
+    const splitString = str.split(""); 
+
+    console.log('Parola splittata', splitString);
+ 
+    const reverseArray = splitString.reverse();
+
+    console.log('Parola al contrario', reverseArray);
+ 
+    const joinArray = reverseArray.join("");
+
+    console.log('Parola al contrario unita', joinArray)
+    
+    return joinArray;
+}
+
+reverseString(wordReverse);
+
+
+if (joinArray === wordReverse) {
+    console.log('La parola inserita è palindroma!');
+} else {
+    console.log('la parola inserita non è palindroma!');
+}
