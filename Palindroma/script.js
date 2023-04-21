b9d38e14ce24977daba59df1f88bcef6984ef94a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-21 10:07:15 +0200

    add method with cycle for

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -10,6 +10,7 @@ ALTRIMENTI
 
 const wordReverse = prompt('inserisci una parola e ti dirò se è palindroma!');
 
+/*
 function reverseString(str) {
     const splitString = str.split(""); 
 
@@ -25,8 +26,32 @@ function reverseString(str) {
     
     return joinArray;
 }
+*/
+
+function isPalindrom() {
+    let reversedWord = ''
+    for (let i = wordReverse.length - 1; i >= 0; i--) {
+        
+        reversedWord += wordReverse[i];
+    }
+
+    if (wordReverse == reversedWord) {
+        return true;
+    }
+    return false;
+}
 
 
+if (isPalindrom(wordReverse)) {
+    console.log('La parola inserita è palindroma!');
+    document.getElementById('result').innerHTML = 'La parola inserita è palindroma!';
+} else {
+    console.log('la parola inserita non è palindroma!');
+    document.getElementById('result').innerHTML = 'La parola inserita non è palindroma!';
+}
+
+
+/*
 if (reverseString(wordReverse) === wordReverse) {
     console.log('La parola inserita è palindroma!');
     document.getElementById('result').innerHTML = 'La parola inserita è palindroma!';
@@ -34,3 +59,4 @@ if (reverseString(wordReverse) === wordReverse) {
     console.log('la parola inserita non è palindroma!');
     document.getElementById('result').innerHTML = 'La parola inserita non è palindroma!';
 }
+*/
